53cce45c3f940b90a7d82a36fe549606b89625ba
Mina Samy Hannalla <minasamy9090.ms@gmail.com>
2022-11-19 17:33:09 +0200

    Sections

--- Frontend/my-app/src/components/courseSections/SectionItem.js
+++ Frontend/my-app/src/components/courseSections/SectionItem.js
@@ -1,28 +1,62 @@
 import React from 'react'
 import { useState } from 'react'
-const SectionItem = () => {
+const SectionItem = ({ text, description }) => {
     const [hidden, setHidden] = useState(true);
     if (hidden)
         return (
-            <div onClick={() => setHidden(false)}>
-                <div class="tab w-full overflow-hidden border-t" >
-                    <input class="absolute opacity-0" id="tab-single-one" />
-                    <label class="block p-5 leading-normal cursor-pointer" for="tab-single-one">Label One</label>
-                    <div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal hidden" >
-                        <p class="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
-                    </div>
+
+            <div class="bg-white border border-gray-200 hover:border-blue-500" onClick={() => setHidden(false)}>
+
+                <h2 class=" mb-0" id="headingOne">
+
+                    <button class="
+                            relative
+                            flex
+                            items-center
+                            w-full
+                            py-4
+                            px-5
+                            text-xl
+                            font-bold text-blue-500  text-left
+                            bg-white
+                            border-0
+                            rounded-none
+                            transition
+                            focus:outline-none"
+                        type="button">
+                        {text}
+                    </button>
+                </h2>
+
+                <div class="py-4 px-5 hidden">
+                    {description}
                 </div>
-            </div >
+            </div>
         )
     else
         return (
-            <div>
-                <div class="tab w-full overflow-hidden border-t" onClick={() => setHidden(true)}>
-                    <input class="absolute opacity-0" id="tab-single-one" />
-                    <label class="block p-5 leading-normal cursor-pointer" for="tab-single-one">Label One</label>
-                    <div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal ">
-                        <p class="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
-                    </div>
+            <div class="bg-white border border-gray-200 hover:border-blue-500" onClick={() => setHidden(true)}>
+                <h2 class=" mb-0" id="headingOne">
+                    <button class="
+                            relative
+                            flex
+                            items-center
+                            w-full
+                            py-4
+                            px-5
+                            text-xl
+                            font-bold text-blue-800 text-left
+                            bg-white
+                            border-0
+                            rounded-3xl
+                            transition
+                            focus:outline-none"
+                        type="button">
+                        {text}
+                    </button>
+                </h2>
+                <div class="py-4 px-5 cursor-pointer">
+                    {description}
                 </div>
             </div>
         )
