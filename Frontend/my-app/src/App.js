a291c87438f49cc94056d64e76c4a467a7bc9b62
Mina Samy Hannalla <minasamy9090.ms@gmail.com>
2022-11-19 03:11:33 +0200

    FAQs

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -2,11 +2,16 @@ import Navbar from "./components/navbar/Navbar";
 import Homepage from './components/homepage/Homepage'
 import Footer from "./components/footer/Footer";
 import Reviews from "./components/reviews/Reviews";
+import Sections from "./components/courseSections/Sections";
+import Faqs from "./components/faq/Faqs";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Homepage />
+      <Sections />
+      <Faqs />
       <Reviews />
       <Footer />
     </div>
---
+++ Frontend/my-app/src/components/courseSections/Sections.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Sections = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Sections
