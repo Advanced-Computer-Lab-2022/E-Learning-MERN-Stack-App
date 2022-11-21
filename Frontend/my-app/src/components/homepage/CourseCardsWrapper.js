e0cb8ca13229798d62389e06076206e5c9152eed
Mina Samy Hannalla <minasamy9090.ms@gmail.com>
2022-11-22 01:33:27 +0200

    Course's Card Customization

--- Frontend/my-app/src/components/homepage/CourseCardsWrapper.js
+++ Frontend/my-app/src/components/homepage/CourseCardsWrapper.js
@@ -4,7 +4,7 @@ const CourseCardsWrapper = ({ children, headingText }) => {
     return (
         <>
 
-            <h1 className=' px-10 py-3 font-bold text-lg  '> {headingText} </h1>
+            <h1 className=' px-10 py-3 font-bold text-lg  '> {headingText} <span className='text-gray-500 font-thin text-base'> (Best Sellers)</span></h1>
             <div className='p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5'>
                 {children}
             </div>
