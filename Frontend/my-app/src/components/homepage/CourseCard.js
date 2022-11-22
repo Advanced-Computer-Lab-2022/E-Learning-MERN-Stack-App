1d4b370d03a9e94978b6a0cbf45615d412447069
Mina Samy Hannalla <minasamy9090.ms@gmail.com>
2022-11-22 23:29:34 +0200

    Update Course Demo Image

--- Frontend/my-app/src/components/homepage/CourseCard.js
+++ Frontend/my-app/src/components/homepage/CourseCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CourseCard = ({ courseName, courseDetails, coursePrice }) => {
     return (
         <div class="shadow-lg rounded-2xl w-96 p-4 bg-gray-50 hover:bg-blue-50 relative overflow-hidden cursor-pointer">
-            <img alt="moto" src="./img/courseimg.png" class="absolute -right-20 -bottom-10 h-40 w-40 mb-4" />
+            <img alt="moto" src="./svg/courseDemoIcon.svg" class="absolute -right-5 -bottom-10 h-40 w-40 mb-4" />
             <div class="w-4/6">
                 <p class="text-gray-800 text-lg font-bold mb-2">
                     {courseName}
