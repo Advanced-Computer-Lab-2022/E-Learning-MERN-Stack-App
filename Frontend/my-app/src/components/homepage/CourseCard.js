3910a5c0cb11555e3e64974a2f39c0f720b0586d
Mina Samy Hannalla <minasamy9090.ms@gmail.com>
2022-11-24 00:18:45 +0200

    Added Course View ~ Access

--- Frontend/my-app/src/components/homepage/CourseCard.js
+++ Frontend/my-app/src/components/homepage/CourseCard.js
@@ -1,23 +1,24 @@
 import React from 'react'
-
+import { useContext } from 'react'
+// import CourseContext from '../../context/CourseInfoContext'
+import CurrentViewContext from '../../context/CurrentViewContext'
 const CourseCard = ({ courseName, courseDetails, coursePrice }) => {
+    const { setView } = useContext(CurrentViewContext);
     return (
-        <a href='/course'>
-            <div class="shadow-lg rounded-2xl w-96 p-4 bg-gray-50 hover:bg-blue-50 relative overflow-hidden cursor-pointer">
-                <img alt="moto" src="./svg/courseDemoIcon.svg" class="absolute -right-5 -bottom-10 h-40 w-40 mb-4" />
-                <div class="w-4/6">
-                    <p class="text-gray-800 text-lg font-bold mb-2">
-                        {courseName}
-                    </p>
-                    <p class="text-gray-400 text-xs">
-                        {courseDetails}
-                    </p>
-                    <p class="text-blue-500 text-xl pt-2 font-medium">
-                        ${coursePrice}
-                    </p>
-                </div>
+        <div onClick={() => setView("course")} class="shadow-lg rounded-2xl w-96 p-4 bg-gray-50 hover:bg-blue-50 relative overflow-hidden cursor-pointer">
+            <img alt="moto" src="./svg/courseDemoIcon.svg" class="absolute -right-5 -bottom-10 h-40 w-40 mb-4" />
+            <div class="w-4/6">
+                <p class="text-gray-800 text-lg font-bold mb-2">
+                    {courseName}
+                </p>
+                <p class="text-gray-400 text-xs">
+                    {courseDetails}
+                </p>
+                <p class="text-blue-500 text-xl pt-2 font-medium">
+                    ${coursePrice}
+                </p>
             </div>
-        </a>
+        </div>
     )
 }
 
