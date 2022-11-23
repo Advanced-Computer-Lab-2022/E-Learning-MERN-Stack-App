13817453cd073fefe3856e5ef414d1444e52c6e1
Mina Samy Hannalla <minasamy9090.ms@gmail.com>
2022-11-24 01:35:51 +0200

    Typo

--- Frontend/my-app/src/pages/CheckoutPage.js
+++ Frontend/my-app/src/pages/CheckoutPage.js
@@ -22,7 +22,7 @@ const CheckoutPage = () => {
                                 Pay
                             </button>
                             {/* To be changed to LoggedIn user later */}
-                            <button onClick={() => setView('guest')} type="submit" class="py-2 px-4  bg-gray-500 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                            <button onClick={() => setView('guest')} class="py-2 px-4  bg-gray-500 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                                 Go Back
                             </button>
                         </div>
