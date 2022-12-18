5d6de2d80a1acdb47067f400ddce26fb205ecfea
Nour-aldeen <nouraldeenm0@gmail.com>
2022-12-19 01:15:47 +0200

    Link API to getCourses, still needs work

--- Frontend/my-app/src/components/homepage/Homepage.jsx
+++ Frontend/my-app/src/components/homepage/Homepage.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { useEffect } from 'react';
 // import Card from './Card'
 // import CardsHolder from './CardsHolder'
 // import CardTag from './CardTag'
@@ -6,14 +8,29 @@ import CourseCard from './CourseCard'
 import CourseCardsWrapper from './CourseCardsWrapper'
 import CoursesHolder from './CoursesHolder'
 
-function Homepage({ visible }) {
+const Homepage = ({ visible }) => {
 
+    const [courses, setCourses] = useState([]);
     const [categories, setCategories] = useState([]);
+
+    async function getCourses() {
+        await axios.get('http://localhost:8000/api/getCourses')
+            .then(response => {
+                console.log(response);
+                setCourses(response.data.courses);
+            })
+            .catch(error => console.log(error));
+    }
+
+    useEffect(() => {
+        getCourses();
+        setCategories(Array.from(new Set(courses.map(course => course.category))));
+    }, []);
+
 
     if (visible)
         return (
             <>
-
                 {/* <CardsHolder>
                     <Card
                         text={"Text 1"}
@@ -41,7 +58,17 @@ function Homepage({ visible }) {
                 </CardsHolder> */}
                 <div className='text-center py-10 text-blue-500 text-4xl font-extrabold'>All Categories</div>
                 <CoursesHolder>
-                    <CourseCardsWrapper headingText={"Web Development"}>
+                    {
+                        categories.map(
+                            category =>
+                                <CourseCardsWrapper headingText={category}>
+                                    {
+                                        courses.map(course => <CourseCard courseName={course.name} courseDetails={course.description} coursePrice={course.price} />)
+                                    }
+                                </CourseCardsWrapper>
+                        )
+                    }
+                    {/* <CourseCardsWrapper headingText={"Web Development"}>
                         <CourseCard
                             courseName={"Lorem ipsum"}
                             courseDetails={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas placerat enim a orci mattis, at pellentesque velit feugiat"}
@@ -96,11 +123,18 @@ function Homepage({ visible }) {
                             courseDetails={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas placerat enim a orci mattis, at pellentesque velit feugiat"}
                             coursePrice={9.99}
                         />
-                    </CourseCardsWrapper>
+                    </CourseCardsWrapper> */}
                 </CoursesHolder>
 
             </>
         )
+    // return <div>
+    //     <ul>
+    //         {
+    //             categories.map(category => <li>{category}</li>)
+    //         }
+    //     </ul>
+    // </div>;
 }
 
 export default Homepage
