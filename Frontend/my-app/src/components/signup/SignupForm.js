b8ac44873f8a4f26e864e90ddcf015fb218826e1
Nour-aldeen <nouraldeenm0@gmail.com>
2022-12-19 14:43:20 +0200

    Fix select in singup form

--- Frontend/my-app/src/components/signup/SignupForm.js
+++ Frontend/my-app/src/components/signup/SignupForm.js
@@ -17,7 +17,11 @@ const SignupForm = () => {
         if (accountInfo.password === confirmPasssword)
             await axios.post('http://localhost:8000/api/guest/signup', accountInfo)
                 .then(response => console.log(response))
-                .catch(error => console.log(error));
+                .catch(error => {
+                    console.log(error)
+                    alert("Password is incorrect");
+                    e.target.preventDefault();
+                });
 
     }
 
@@ -72,37 +76,37 @@ const SignupForm = () => {
         <form className="flex flex-col pt-3 md:pt-8">
             <div className="flex flex-col pt-4">
                 <div className="flex relative ">
-                    <input type="text" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Username" onChange={(e) => handleUsernameChange(e)} required />
+                    <input type="text" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Username" onChange={e => handleUsernameChange(e)} required />
                 </div>
             </div>
             <div className="flex flex-col pt-4">
                 <div className="flex relative ">
-                    <input type="email" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Email" onChange={(e) => handleEmailChange(e)} required />
+                    <input type="email" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Email" onChange={e => handleEmailChange(e)} required />
                 </div>
             </div>
             <div className="flex flex-col pt-4">
                 <div className="flex relative ">
-                    <input type="password" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Password" onChange={(e) => handlePasswordChange(e)} id='password' required />
+                    <input type="password" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Password" onChange={e => handlePasswordChange(e)} id='password' required />
                 </div>
             </div>
             <div className="flex flex-col pt-4">
                 <div className="flex relative ">
-                    <input type="password" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Confirm Password" id='confirmPassword' onChange={(e) => handleConfirmPasswordChange(e)} required />
+                    <input type="password" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Confirm Password" id='confirmPassword' onChange={e => handleConfirmPasswordChange(e)} required />
                 </div>
             </div>
             <div className="flex flex-col pt-4">
                 <div className="flex relative ">
-                    <input type="text" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="First Name" onChange={(e) => handleFirstnameChange(e)} required />
+                    <input type="text" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="First Name" onChange={e => handleFirstnameChange(e)} required />
                 </div>
             </div>
             <div className="flex flex-col pt-4">
                 <div className="flex relative ">
-                    <input type="text" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Last Name" onChange={(e) => handleLastnameChange(e)} required />
+                    <input type="text" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Last Name" onChange={e => handleLastnameChange(e)} required />
                 </div>
             </div>
             <div className="flex flex-col pt-4 mb-12">
                 <div className="flex relative">
-                    <select name="country" className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-800 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
+                    <select name="country" className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-800 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" onChange={e => handleCountryChange(e)} >
                         <option disabled selected>Select Country</option>
                         <option value="Afghanistan">Afghanistan</option>
                         <option value="Åland Islands">Åland Islands</option>
