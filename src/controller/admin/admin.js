300db417229f9750fc3aea63a16b0169ea5fb4cc
LoayHesham0 <99695584+LoayHesham0@users.noreply.github.com>
2022-12-25 19:18:55 +0200

    Fix login with same username or email

--- src/controller/admin/admin.js
+++ src/controller/admin/admin.js
@@ -1,7 +1,7 @@
-const Admin = require('../../models/admin');
-const OrgGuest = require('../../models/orgGuest');
-const Instructor = require('../../models/instructor');
-
+const   Guest  = require("../../models/guest");
+const   Instructor  = require("../../models/instructor");
+const  OrgGuest  = require('../../models/orgGuest');
+const  Admin  = require("../../models/admin");
 const jwt = require('jsonwebtoken');
 
 
@@ -12,36 +12,68 @@ exports.isAdmin = (req, res, next) => {
     next();
 
 };
-exports.signup = (req, res) => {
-    Admin.findOne({email: req.body.email})
-   .exec((error, admin) => {
-       if(admin) return res.status(400).json({
-           message: 'User already registered' 
-       });
-
-       const _admin = new Admin({
-           firstName: req.body.firstName,
-           lastName: req.body.lastName,
-           userName: req.body.userName,
-           email: req.body.email,
-           password: req.body.password,
-           country: req.body.country,
-           gender: req.body.gender
-       });
-       _admin.save((error, data) => {
-           if(error){
-           return res.status(400).json({
-               message: 'Something went wrong'
-           });
-       }
-       if(data){
-           return res.status(201).json({
-               message:'Admin created  successfuly..!'
-           })
-       }
-    });
-       
-   });
+exports.signup = async (req, res) => {
+    var user = await Guest.findOne({ userName: req.body.userName });
+    if (user){
+        return res.status(400).send("Username already registered");
+    }else{
+        user = await OrgGuest.findOne({ userName: req.body.userName });
+        if(user){
+            return res.status(400).send("Username already registered");
+        }else{
+            user = await Instructor.findOne({ userName: req.body.userName });
+            if(user){
+                return res.status(400).send("Username already registered");
+            }else{
+                user = await Admin.findOne({ userName: req.body.userName });
+                if(user){
+                    return res.status(400).send("Username already registered");
+                }else{
+                    user = await Guest.findOne({ email: req.body.email });
+                    if(user){
+                        return res.status(400).send("email already registered");
+                    }else{
+                        user = await OrgGuest.findOne({ email: req.body.email });
+                        if(user){
+                            return res.status(400).send("email already registered");
+                        }else{
+                            user = await Instructor.findOne({ email: req.body.email });
+                            if(user){
+                                return res.status(400).send("email already registered");
+                            }else{
+                                user = await Admin.findOne({ email: req.body.email });
+                                if(user){
+                                    return res.status(400).send("email already registered");
+                                }else{
+                                    const _admin = new Admin({
+                                        firstName: req.body.firstName,
+                                        lastName: req.body.lastName,
+                                        userName: req.body.userName,
+                                        email: req.body.email,
+                                        password: req.body.password,
+                                        country: req.body.country,
+                                        gender: req.body.gender
+                                    });
+                                    _admin.save((error, data) => {
+                                        if(error){
+                                        return res.status(400).json({
+                                            message: 'Something went wrong'
+                                        });
+                                    }
+                                    if(data){
+                                        return res.status(201).json({
+                                            message:'Admin created  successfuly..!'
+                                        })
+                                    }
+                                 });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 };
 
 exports.signin = (req, res) => {
@@ -71,47 +103,117 @@ exports.signin = (req, res) => {
     });
 };
 
-exports.addOrgGuest = (req, res) => {
-    OrgGuest.findOne({email: req.body.email})
-    .exec((error, guest) => {
-         if(guest) res.status(400).json({message: "Guest already registered"});
-        });
-        const _guest = new OrgGuest ({
-            email: req.body.email,
-            password: req.body.password,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            company: company._id,
-            userName: req.body.userName,
-            gender: req.body.gender,
-            country: req.body.country,
-            });
-            _guest.save((err, data)=> {
-            if(err) res.status(400).json({message: "Something went wrong"});
-            if(data) res.status(201).json({message: "User was added successfuly...!"});
-        });
+exports.addOrgGuest = async (req, res) => {
+    var user = await Guest.findOne({ userName: req.body.userName });
+    if (user){
+        return res.status(400).send("Username already registered");
+    }else{
+        user = await OrgGuest.findOne({ userName: req.body.userName });
+        if(user){
+            return res.status(400).send("Username already registered");
+        }else{
+            user = await Instructor.findOne({ userName: req.body.userName });
+            if(user){
+                return res.status(400).send("Username already registered");
+            }else{
+                user = await Admin.findOne({ userName: req.body.userName });
+                if(user){
+                    return res.status(400).send("Username already registered");
+                }else{
+                    user = await Guest.findOne({ email: req.body.email });
+                    if(user){
+                        return res.status(400).send("email already registered");
+                    }else{
+                        user = await OrgGuest.findOne({ email: req.body.email });
+                        if(user){
+                            return res.status(400).send("email already registered");
+                        }else{
+                            user = await Instructor.findOne({ email: req.body.email });
+                            if(user){
+                                return res.status(400).send("email already registered");
+                            }else{
+                                user = await Admin.findOne({ email: req.body.email });
+                                if(user){
+                                    return res.status(400).send("email already registered");
+                                }else{
+                                    const _guest = new OrgGuest ({
+                                        email: req.body.email,
+                                        password: req.body.password,
+                                        firstName: req.body.firstName,
+                                        lastName: req.body.lastName,
+                                        company: company._id,
+                                        userName: req.body.userName,
+                                        gender: req.body.gender,
+                                        country: req.body.country,
+                                        });
+                                        _guest.save((err, data)=> {
+                                        if(err) res.status(400).json({message: "Something went wrong"});
+                                        if(data) res.status(201).json({message: "User was added successfuly...!"});
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 };
 
-exports.addInstructor = (req, res) => {
-    Instructor.findOne({email: req.body.email})
-    .exec((error, instructor) =>{
-        if(instructor) res.status(400).json({message: "Instructor already registered"});
-        else{
-            const _instructor = new Instructor({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: req.body.password,
-                userName: req.body.userName,
-                gender: req.body.gender,
-                country: req.body.country,
-            });
-            _instructor.save((error, data)=>{
-                if(error) res.status(400).json({message: "Something went wrong"});
-               if(data) res.status(201).json({message: "Instructor was added successfuly...!"});
-            });
+exports.addInstructor = async (req, res) => {
+    var user = await Guest.findOne({ userName: req.body.userName });
+    if (user){
+        return res.status(400).send("Username already registered");
+    }else{
+        user = await OrgGuest.findOne({ userName: req.body.userName });
+        if(user){
+            return res.status(400).send("Username already registered");
+        }else{
+            user = await Instructor.findOne({ userName: req.body.userName });
+            if(user){
+                return res.status(400).send("Username already registered");
+            }else{
+                user = await Admin.findOne({ userName: req.body.userName });
+                if(user){
+                    return res.status(400).send("Username already registered");
+                }else{
+                    user = await Guest.findOne({ email: req.body.email });
+                    if(user){
+                        return res.status(400).send("email already registered");
+                    }else{
+                        user = await OrgGuest.findOne({ email: req.body.email });
+                        if(user){
+                            return res.status(400).send("email already registered");
+                        }else{
+                            user = await Instructor.findOne({ email: req.body.email });
+                            if(user){
+                                return res.status(400).send("email already registered");
+                            }else{
+                                user = await Admin.findOne({ email: req.body.email });
+                                if(user){
+                                    return res.status(400).send("email already registered");
+                                }else{
+                                    const _instructor = new Instructor({
+                                        firstName: req.body.firstName,
+                                        lastName: req.body.lastName,
+                                        email: req.body.email,
+                                        password: req.body.password,
+                                        userName: req.body.userName,
+                                        gender: req.body.gender,
+                                        country: req.body.country,
+                                    });
+                                    _instructor.save((error, data)=>{
+                                        if(error) res.status(400).json({message: "Something went wrong"});
+                                       if(data) res.status(201).json({message: "Instructor was added successfuly...!"});
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-    })
+    }
 };
 
 
