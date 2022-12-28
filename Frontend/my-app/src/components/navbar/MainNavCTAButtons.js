2150ea8f0e126841f6bf063e566c8084f3799c0c
Nour-aldeen <nouraldeenm0@gmail.com>
2022-12-28 22:35:58 +0200

    Revert prev change in MainNavCTAButtons

--- Frontend/my-app/src/components/navbar/MainNavCTAButtons.js
+++ Frontend/my-app/src/components/navbar/MainNavCTAButtons.js
@@ -11,17 +11,17 @@ const MainNavCTAButtons = () => {
         <>
             {navIdx !== 5 ? [<div onClick={() => {
                 setNavIdx(5)
-                // if (user.username === undefined)
-                //     setView('guest')
-                // else
-                //     setView('user')
+                if (user.username === undefined)
+                    setView('guest')
+                else
+                    setView('user')
             }} className="hidden cursor-pointer lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200">Sign In</div>] : []}
             {navIdx !== 6 ? [<div onClick={() => {
                 setNavIdx(6)
-                // if (user.username === undefined)
-                //     setView('guest')
-                // else
-                //     setView('user')
+                if (user.username === undefined)
+                    setView('guest')
+                else
+                    setView('user')
             }} className="hidden cursor-pointer lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200">Sign up</div>] : []}
           
         </>
