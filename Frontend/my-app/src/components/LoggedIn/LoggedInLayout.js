92d83c0e5ba0a381c236a6a85536ad1686750749
Nour-aldeen <nouraldeenm0@gmail.com>
2022-12-28 22:32:10 +0200

    User Can now log out

--- Frontend/my-app/src/components/LoggedIn/LoggedInLayout.js
+++ Frontend/my-app/src/components/LoggedIn/LoggedInLayout.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import NavBarLogo from '../navbar/NavBarLogo'
 import MainNavList from '../navbar/MainNavList'
 import Footer from '../footer/Footer'
 import ProfileIconNavBar from './ProfileIconNavBar'
+import NavStateContext from '../../context/NavStateContext'
+import CurrentViewContext from '../../context/CurrentViewContext'
+import UserInfoContext from '../../context/UserInfoContext'
+import { Cookies, useCookies } from 'react-cookie'
+
 const LoggedInLayout = ({ children }) => {
+    const { navIdx, setNavIdx } = useContext(NavStateContext);
+    const { setView } = useContext(CurrentViewContext);
+    const { user, setUser } = useContext(UserInfoContext);
+    const [removeCookie] = useCookies(['userCookie'])
+
+    function handleSignOut() {
+        new Cookies().remove('userCookie');
+        new Cookies().remove('token');
+        window.location.reload()
+    }
+
     return (
         <>
             <nav className="relative px-4 py-4 flex justify-between items-center bg-white">
                 <NavBarLogo />
                 <MainNavList />
                 <ProfileIconNavBar />
+                <div onClick={() => {
+                    handleSignOut();
+                }} className="hidden cursor-pointer lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200">Sign out</div>
             </nav>
             {children}
             <Footer />
