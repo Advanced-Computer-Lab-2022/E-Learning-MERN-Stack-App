7df524468dbec7fb6a4c921350a33dc24f04ff86
Nour-aldeen <nouraldeenm0@gmail.com>
2022-12-28 22:43:52 +0200

    Log out button finished

--- Frontend/my-app/src/components/LoggedIn/LoggedInLayout.js
+++ Frontend/my-app/src/components/LoggedIn/LoggedInLayout.js
@@ -26,10 +26,12 @@ const LoggedInLayout = ({ children }) => {
                 <NavBarLogo />
                 <MainNavList />
                 <ProfileIconNavBar />
+            </nav>
+            <div className='flex m-5'>
                 <div onClick={() => {
                     handleSignOut();
-                }} className="hidden cursor-pointer lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200">Sign out</div>
-            </nav>
+                }} className="hidden cursor-pointer lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold  rounded-xl transition duration-200">Sign out</div>
+            </div>
             {children}
             <Footer />
         </>
